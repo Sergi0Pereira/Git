884771b294750fc813c566ef4096ca1733c675d0
Sergio <tpessoalsp@gmail.com>
2022-12-26 15:31:15 +0000

    ordername adicionado

---
+++ orderName.py
@@ -0,0 +1,14 @@
+[200~#program begins
+
+names = input(Please enter a list of names: )
+
+#split names into a list
+name_list = names.split(, )
+
+#sort names alphabetically
+name_list.sort()
+
+#print sorted list
+print(name_list)
+
+#program ends~
